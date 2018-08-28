b5bffd5f0959a98063505d2a7ca1187659aa2b0b
Cedric David <cedric.david@btopenworld.com>
2018-08-28 12:55:13 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
     boardPos = board[index-1]
   if boardPos === " " || boardPos === "" ||boardPos === nil
     false
-  elsif boardPos === "X" || boardPos === "O"
+  else
     true
   end
 end
