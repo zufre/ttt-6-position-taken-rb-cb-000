29ab3605993ccad51f957cf92bdd101316fbfc5b
Cedric David <cedric.david@btopenworld.com>
2018-08-28 12:41:56 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-    boardPos = board[index-1] 
+    boardPos = board[index-1]
   if boardPos === " " || boardPos === nil
     false
   elsif boardPos === "X" || boardPos ==="O"
-    
+    true
 
 
 
