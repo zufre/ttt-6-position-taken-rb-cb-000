154516d3d7fbf518104e79fd74f4b272f83ebd4c
Cedric David <cedric.david@btopenworld.com>
2018-08-28 12:56:18 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    boardPos = board[index]
+  if boardPos === " " || boardPos === "" ||boardPos === nil
+    false
+  elsif boardPos === "X" || boardPos === "O"
+    true
+  end
+end
